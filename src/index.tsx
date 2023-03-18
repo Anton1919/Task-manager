dbb01742da643b2d71c108d723d30bd9058a82cc
Anton1919 <bf3kenzo@mail.ru>
2023-03-18 18:55:26 +0300

    completed the second toolkit lesson

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,6 @@ import {Provider} from 'react-redux';
 import {store} from './app/store';
 import {HashRouter} from "react-router-dom";
 
-
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 );
